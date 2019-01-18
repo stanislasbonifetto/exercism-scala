aeae0fcbad5441307ea0434c55b8dd3c54c50f61
Stanislas Bonifetto <stanislas.bonifetto@argos.co.uk>
2019-01-18 21:56:51 +0000

    solve the leap

--- leap/src/main/scala/Leap.scala
+++ leap/src/main/scala/Leap.scala
@@ -1,3 +1,9 @@
 object Leap {
-  def leapYear(year: Int): Boolean = ???
+  def leapYear(year: Int): Boolean = {
+    year match {
+      case n if n % 400 == 0 => true
+      case n if n % 4 == 0 && n % 100 != 0 => true
+      case _ => false
+    }
+  }
 }
